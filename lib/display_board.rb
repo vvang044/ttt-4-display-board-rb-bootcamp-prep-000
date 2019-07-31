1a700be7d0101609e502b471ce3bbd1e3fcc1e37
Vik Vangala <vvang044@gmail.com>
2019-07-31 12:48:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -25,7 +25,7 @@ end
 def display_board (board)
   puts "#{board[0]}   |   |   "
   puts "-----------"
-  puts "   | #{board[4]} |   "
+  puts "   |  |   "
   puts "-----------"
   puts "   |   |   "
 
