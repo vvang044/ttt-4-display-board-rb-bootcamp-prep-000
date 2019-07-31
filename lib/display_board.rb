363b064d42d526495682dadfd9a63fd25450c78d
Vik Vangala <vvang044@gmail.com>
2019-07-31 13:39:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -47,4 +47,12 @@ def display_board (board)
   
 end
 
-
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def display_board (board)
+  puts " #{board[0]} | #{board[4]} | #{board[4]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
+end
