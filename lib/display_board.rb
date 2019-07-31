3c688b7555df5a52f65a50838cd1f2cfd5e04f72
Vik Vangala <vvang044@gmail.com>
2019-07-31 12:58:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,18 +12,4 @@ def display_board (board)
   puts "   |   |   "
 end
 
-def display_board (board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | #{board[4]} |   "
-  puts "-----------"
-  puts "   |   |   "
-end
 
-def display_board (board)
-  puts " #{board[0]}  |   |   "
-  puts "-----------"
-  puts "   |  |   "
-  puts "-----------"
-  puts "   |   |   "
-end
